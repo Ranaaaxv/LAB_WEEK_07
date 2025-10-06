9db2421dd26533686ba67e4a9c8e036ad8124879
Ranaaaxv <vanessaaudrelia@gmail.com>
2025-10-06 17:25:01 +0700

    Commit No. 3 – Move Google Maps API Key to secure location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,11 @@ android {
     }
 }
 
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+}
+
 dependencies {
 
     implementation(libs.androidx.activity.ktx)
